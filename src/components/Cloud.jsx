429353d3c51da72b4758a719968dfcd506538317
AspiringWebGaurav <gauravpatil5737@gmail.com>
2023-09-07 11:33:29 +0530

    main slider top margin and cloud below main slider not looking allign fixed

--- src/components/Cloud.jsx
+++ src/components/Cloud.jsx
@@ -7,11 +7,11 @@ import cloud2 from '../assets/images/cloud2.png'
 const Cloud = () => {
   return (
     <div>
-      <div className="top-cloud1">
-        <img src={cloud1} alt="Cloud 1" />
-      </div>
       <div className="top-cloud">
         <img src={cloud2} alt="Cloud 2" />
+      </div>
+      <div className="top-cloud1">
+        <img src={cloud1} alt="Cloud 1" />
       </div>
     </div>
   )
