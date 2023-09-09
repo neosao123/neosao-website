31bff9070490a23b68211c43fad75f9e0cc6005c
AspiringWebGaurav <gauravpatil5737@gmail.com>
2023-09-09 13:08:22 +0530

    Navbar responsiveness Done

--- src/components/Offcanvas.jsx
+++ src/components/Offcanvas.jsx
@@ -1,5 +1,9 @@
 
 import React, { useState } from 'react';
+import logo from '../assets/images/logo-icon.png'
+import '../assets/style/offcanvas.css'
+import { NavLink } from 'react-router-dom';
+import { Link } from 'react-scroll'
 
 function Offcanvas() {
     const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(false);
@@ -10,25 +14,33 @@ function Offcanvas() {
 
     return (
         <>
-
             <button onClick={toggleOffcanvas} className="navbar-toggler collapsed">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-            {/* <button className="btn btn-primary" type="button" onClick={toggleOffcanvas}>
-                Toggle right offcanvas
-            </button> */}
-
             <div className={`offcanvas offcanvas-end${isOffcanvasOpen ? ' show' : ''}`} tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
                 <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+                    <a className="logo" href="/neosaoweb">
+                        <h2><img src={logo} alt="Logo" /> <span>Neosao</span></h2>
+                    </a>
                     <button type="button" className="btn-close" onClick={toggleOffcanvas} aria-label="Close"></button>
                 </div>
                 <div className="offcanvas-body">
-                    {/* ... Content for the offcanvas body */}
+                    {isOffcanvasOpen && (
+                        <ul className='offcanvas-menu offcanvas-custom-link vertical-menu'>
+                            <Link to='home-main' className='offcanvas-custom-link' spy={true} smooth={true} offset={-100} duration={500}>HOME</Link>
+                            <Link to='about' className='offcanvas-custom-link' spy={true} smooth={true} offset={50} duration={500}>ABOUT</Link>
+                            <Link to='services' className='offcanvas-custom-link' spy={true} smooth={true} offset={50} duration={500}>SERVICES</Link>
+                            <NavLink to='/portfolio' className='offcanvas-custom-link'>PORTFOLIO</NavLink>
+                            <Link to='career' className='offcanvas-custom-link' spy={true} smooth={true} offset={2} duration={500}>CAREER</Link>
+                            <NavLink to='/reach-us-contact' className="offcanvas-contact_btn">Reach us</NavLink>
+                        </ul>
+
+                    )}
                 </div>
             </div>
         </>
+
     );
 }
 export default Offcanvas;
