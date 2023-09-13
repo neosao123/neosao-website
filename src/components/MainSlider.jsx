cf62c410e2965e60c21c17c02aa18ba40a72b260
AspiringWebGaurav <gauravpatil5737@gmail.com>
2023-09-13 23:26:04 +0530

    MainSlider is now active achieved using SWIPERJs but not alble to use fadein fadeOut effect instead used sliding-Autoplay

--- src/components/MainSlider.jsx
+++ src/components/MainSlider.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../assets/style/mainslider.css'
-import '../assets/style/Responsiveness.css'
+// import '../assets/style/Responsiveness.css'
 import '../assets/style/animation.css'
 import justBanner from '../assets/images/just_banner.png'
 import justBanner1 from '../assets/images/just_banner1.png'
@@ -13,13 +13,115 @@ import star1 from '../assets/images/star-1.png'
 import star2 from '../assets/images/star-2.png'
 import b4six from '../assets/images/b4-dot-6.png'
 
+// SWIPER
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css/bundle';
+// Import Swiper styles
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+
+// EFFECT
+
+
 
 const MainSlider = () => {
   return (
+
     <div className="main-slider" id="index">
-      <div className="slider">
-        
-        
+
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+
+        autoplay={{ delay: 4000 }}
+      // effect='fade'
+
+      >
+
+
+        {/* Slide 1 */}
+        <SwiperSlide>
+          <div>
+            <div className="slider-img animatedParent">
+              <img src={justBanner} alt="Banner 1" />
+              <div className="slider-text">
+                <div className="container">
+                  <h2 className="animated fadeInRight go">Quality, Flexibility,<br /> Transparency, Customizability.</h2>
+                  <p className="animated fadeInUpShort go">Great solution for your business's any software requirements.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+
+
+        {/* Slide 2 */}
+        <SwiperSlide>
+          <div>
+            <div className="slider-img animatedParent">
+              <img src={justBanner1} alt="Banner 2" />
+              <div className="slider-text">
+                <div className="container">
+                  <h2 className="animated fadeInRight go">We Suggest Best.</h2>
+                  <p className="animated fadeInUpShort go">Come up with a basic idea and requirements only.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+
+
+        {/* Slide 3 */}
+        <SwiperSlide>
+          <div>
+            <div className="slider-img animatedParent">
+              <img src={justBanner2} alt="Banner 3" />
+              <div className="slider-text">
+                <div className="container">
+                  <h2 className="animated fadeInRight go">We Serve Best.</h2>
+                  <p className="animated fadeInUpShort go">We are committed to serving you best.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+
+
+        {/* Slide 4 */}
+        <SwiperSlide>
+          <div>
+            <div className="slider-img animatedParent">
+              <img src={justBanner4} alt="Banner 5" />
+              <div className="slider-text">
+                <div className="container">
+                  <h2 className="animated fadeInRight go">We Deploy Best.</h2>
+                  <p className="animated fadeInUpShort go">You will have access to all updates and free support.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+
+      {/* SWIPER END */}
+
+
+
+
+
+
+      {/* ORIGINAL */}
+      {/* <div className="slider">
+
+
         <div>
           <div className="slider-img animatedParent">
             <img src={justBanner} alt="Banner 1" />
@@ -34,19 +136,19 @@ const MainSlider = () => {
 
 
 
-        {/* <div>
+        <div>
           <div className="slider-img animatedParent">
             <img src={justBanner1} alt="Banner 2" />
             <div className="slider-text">
               <div className="container">
                 <h2 className="animated fadeInRight go">We Suggest Best.</h2>
                 <p className="animated fadeInUpShort go">Come up with a basic idea and requirements only.</p>
-                
+
               </div>
             </div>
           </div>
         </div>
-        
+
         <div>
           <div className="slider-img animatedParent">
             <img src={justBanner2} alt="Banner 3" />
@@ -54,7 +156,7 @@ const MainSlider = () => {
               <div className="container">
                 <h2 className="animated fadeInRight go">We Serve Best.</h2>
                 <p className="animated fadeInUpShort go">We are committed to serving you best.</p>
-                
+
               </div>
             </div>
           </div>
@@ -66,13 +168,13 @@ const MainSlider = () => {
               <div className="container">
                 <h2 className="animated fadeInRight go">We Promote Best.</h2>
                 <p className="animated fadeInUpShort go">We have the master key to promote your business.</p>
-                
+
               </div>
             </div>
           </div>
         </div>
-        
-        
+
+
         <div>
           <div className="slider-img animatedParent">
             <img src={justBanner4} alt="Banner 5" />
@@ -80,17 +182,16 @@ const MainSlider = () => {
               <div className="container">
                 <h2 className="animated fadeInRight go">We Deploy Best.</h2>
                 <p className="animated fadeInUpShort go">You will have access to all updates and free support.</p>
-                
+
               </div>
             </div>
           </div>
-        </div> */}
+        </div>
 
 
-      </div>
-      
-      
-      
+      </div> */}
+      {/* ORIGINAL */}
+
       <div className="shap1">
         <img src={shape5} alt="Shape 1" />
       </div>
@@ -100,13 +201,13 @@ const MainSlider = () => {
       <div className="star2">
         <img src={star2} alt="Star 2" />
       </div>
-      {/* <div className="dot-6">
+      <div className="dot-6">
         <img src={b4six} alt="Dot 6" />
-      </div> */}
+      </div>
 
     </div>
 
-    
+
   )
 }
 
